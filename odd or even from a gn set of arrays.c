5315cb0ed3397790253eb5d22e8274b52e552812
Saranyasaravanan2003 <112238580+Saranyasaravanan2003@users.noreply.github.com>
2023-04-27 10:30:28 +0530

    Update odd or even from a gn set of arrays.c

--- odd or even from a gn set of arrays.c
+++ odd or even from a gn set of arrays.c
@@ -6,23 +6,17 @@ void main()
     scanf("%d", &n);
  
     int arr[n];
- 
-    //Take n elements as input from the user
     printf("Enter %d elements in the array: ",n);
     for(int i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
- 
-    //Print all the even numbers
     printf("Even numbers in the array are: ");
     for(int i=0;i<n;i++)
     {
         if(arr[i]%2==0)
             printf("%d ", arr[i]);
     }
- 
-    //print all the odd numbers
     printf("\nOdd numbers in the array are: ");
     for(int i=0;i<n;i++)
     {
